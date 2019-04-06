f7faaf97c1cda15731b8546e9b054d4eb7856111
syj <495054021@qq.com>
2019-04-06 10:08:02 +0800

    test

--- src/main/java/com/syj/demo/thread/ThreadTest.java
+++ src/main/java/com/syj/demo/thread/ThreadTest.java
@@ -46,7 +46,7 @@ public class ThreadTest {
                 }
             }
             queue.add("1");
-            System.out.println("=======添加了1个元素===========");
+            System.out.println("=======添加了1个元素=====test======");
             notEmpty.notifyAll();
         }
 
