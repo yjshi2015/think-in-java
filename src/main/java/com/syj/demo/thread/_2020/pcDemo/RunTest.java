e0af1c7e28ea4b890bb81d0a0ff05b3071ef1b28
shiyongjun1 <syj^E^M^2015>
2020-06-06 23:01:08 +0800

    add 注释说明

--- src/main/java/com/syj/demo/thread/_2020/pcDemo/RunTest.java
+++ src/main/java/com/syj/demo/thread/_2020/pcDemo/RunTest.java
@@ -4,6 +4,10 @@ package com.syj.demo.thread._2020.pcDemo;
  * @Description 线程启动入口
  * @Date 2020/6/6 22:32
  * @Created by shiyongjun1
+ *
+ * 使用生产、消费模式，需要注意2点：
+ * 1.不能使用notify()，必须使用notifyAll()，避免只唤醒了同类（比如消费者），造成所有线程都在wait，从而假死
+ * 2.不能使用if条件作为判断线程的执行条件，因为线程被唤醒后，执行条件有可能发生变化
  */
 public class RunTest {
 
